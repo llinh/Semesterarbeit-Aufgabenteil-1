403b3f287e75d38638182039defe284db8448bae
llinh <36013098+llinh@users.noreply.github.com>
2018-09-27 13:23:24 +0200

    Add files via upload

---
+++ xmlmodel.h
@@ -0,0 +1,14 @@
+#ifndef XMLMODEL_H
+#define XMLMODEL_H
+#include <QtCore>
+#include <product.h>
+
+class XMLModel
+{
+public:
+    XMLModel();
+    QList<product> productList;
+};
+
+
+#endif // XMLMODEL_H
