49c6e17ef14faaf77d1e0e55eac7c62d3e0ba98c
llinh <36013098+llinh@users.noreply.github.com>
2018-09-27 13:24:11 +0200

    Add files via upload

---
+++ xmlparser.h
@@ -0,0 +1,16 @@
+#ifndef XMLPARSER_H
+#define XMLPARSER_H
+
+#include <QObject>
+#include <QDebug>
+#include <QXmlStreamReader>
+#include <QFile>
+#include <QDir>
+
+class XMLParser
+{
+public:
+    XMLParser();
+};
+
+#endif // XMLPARSER_H
